67286fedf77f09d0088ca3dbf182bbef7c3c7238
nedsociety <nedsociety@users.noreply.github.com>
2022-09-09 16:43:40 +0900

    ci: check addon changes once per day

--- snippets/fetch_metadata.py
+++ snippets/fetch_metadata.py
@@ -1,4 +1,5 @@
 import shutil
+import sys
 from pathlib import Path
 from zipfile import ZipFile
 from mvinstaller.fstools import extract_without_path
@@ -12,15 +13,17 @@ def check_and_overwrite(src, dst):
     if not dst.is_file():
         logger.info('-> new addon')
         shutil.move(src, dst)
+        return True
     else:
         with open(src, 'rb') as srcf:
             with open(dst, 'rb') as dstf:
                 if srcf.read() == dstf.read():
                     logger.info('-> no change on metadata.xml')
-                    return
+                    return False
     
         logger.info('-> updating to a new metadata.xml')
         shutil.move(src, dst)
+        return True
 
 def fetch_metadata(addon):
     # Use the first URL
@@ -31,15 +34,18 @@ def fetch_metadata(addon):
     with ZipFile(build_dir / fn) as zipf:
         extract_without_path(zipf, 'mod-appendix/metadata.xml', build_dir)
         
-    check_and_overwrite(build_dir / 'metadata.xml', f'addon_metadata/{addon.metadata_name}.xml')
+    return check_and_overwrite(build_dir / 'metadata.xml', f'addon_metadata/{addon.metadata_name}.xml')
     
 def main():
+    check_mode = (len(sys.argv) >= 2) and (sys.argv[1] == '--check')
     for addon in AddonsList:
         addon = addon.value
         if addon.custom_metadata:
             logger.info(f'[{addon.metadata_name}] Skipped, using custom metadata...')
             continue
         logger.info(f'[{addon.metadata_name}] Fetching...')
-        fetch_metadata(addon)
+        changed = fetch_metadata(addon)
+        if changed and check_mode:
+            raise RuntimeError('Check mode: the metadata files are outdated.')
 
 main()
