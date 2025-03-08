2d0cd4f2c456a6ac650fc87f6f2adb2c12ccc410
Aleev141 <roman.galeev.2016@mail.ru>
2025-03-08 17:23:11 +0500

    Update Fusion

--- mvinstaller/signatures.py
+++ mvinstaller/signatures.py
@@ -439,13 +439,13 @@ class FixedAddonsList(Enum):
         id='Fusion',
         modname='Fusion',
         download_targets={
-            'https://github.com/MV-Fusion-Team/FTL-Multiverse-Fusion/releases/download/v0.1.2/Fusion.zip':
+            'https://github.com/MV-Fusion-Team/FTL-Multiverse-Fusion/releases/download/v0.1.3/Fusion.zip':
                 'Fusion.zip'
         },
-        version='0.1.2',
+        version='0.1.3',
         locale='en',
         metadata_url=None,
-        compatible_mv_locale=['ru'],
+        compatible_mv_locale=[],
         dependent_modnames=[],
         priority=103
     )
